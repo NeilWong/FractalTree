cde7c6c65164a073dde94e9a1d6064a831f45131
NeilWong <nwong1250@gmail.com>
2015-01-29 23:21:54 -0800

    Neil

--- build-tmp/source/FractalTree.java
+++ build-tmp/source/FractalTree.java
@@ -1,16 +1,32 @@
-private double fractionLength = .80; 
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class FractalTree extends PApplet {
+
+private double fractionLength = .80f; 
 private int smallestBranch = 10; 
-private double branchAngle = .3;  
+private double branchAngle = .3f;  
 public void setup() 
 {   
-	size(640,480);    
+	size(640,550);    
 	noLoop(); 
 } 
 public void draw() 
 {   
 	background(0);   
 	stroke(0,255,0);   
-	line(320,480,320,380);   
+	line(320,480,320,420);   
 	drawBranches(320,380,100,3*Math.PI/2);  //will add later 
 } 
 public void drawBranches(int x,int y, double branchLength, double angle) 
@@ -38,3 +54,12 @@ public void drawBranches(int x,int y, double branchLength, double angle)
 
 		
 } 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "FractalTree" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
